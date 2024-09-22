b1d9ef9de704b8dbf9fa28d87044eb688c173dba
ginathethird <chijingna@hotmail.sg>
2024-09-22 21:40:44 +0800

    add bgd and overlay text

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,6 @@ function App() {
       value={{ currencyFrom, setCurrencyFrom, currencyTo, setCurrencyTo }}
     >
       <>
-        <img src="https://images.unsplash.com/photo-1665652475985-37e285aeff53?q=80&w=3431&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-        <h1>Finance Dashboard</h1>
         <StockInput />
       </>
     </StockContext.Provider>
